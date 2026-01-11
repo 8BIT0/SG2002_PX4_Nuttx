62a25171cfcb2f454a40e5712b366392eb95e711
8BIT0 <971168718@qq.com>
2026-01-11 21:21:02 +0800

    Changes to be committed: 	modified:   nuttx/boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c
    1. I2C driver update (none tested)

--- nuttx/boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c
+++ nuttx/boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c
@@ -6,21 +6,21 @@
 #include "sg200x.h"
 
 #if defined(CONFIG_I2C) && defined(CONFIG_SYSTEM_I2CTOOL)
-    static void sg2002_i2c_register(int bus) {
-        struct i2c_master_s *i2c = sg2002_i2cbus_initialize(bus);
+static void sg2002_i2c_register(int bus) {
+    struct i2c_master_s *i2c = sg2002_i2cbus_initialize(bus);
 
-        if (i2c)
-            i2c_register(i2c, bus);
-    }
+    if (i2c)
+        i2c_register(i2c, bus);
+}
 
-    static void sg2002_i2ctool(void) {
-    #ifdef CONFIG_SG2002_I2C1
-        sg2002_i2c_register(1);
-    #endif
-    #ifdef CONFIG_SG2002_I2C3
-        sg2002_i2c_register(3);
-    #endif
-    }
+static void sg2002_i2ctool(void) {
+#ifdef CONFIG_SG2002_I2C1
+    sg2002_i2c_register(1);
+#endif
+#ifdef CONFIG_SG2002_I2C3
+    sg2002_i2c_register(3);
+#endif
+}
 #endif
 
 int sg2002_bringup(void) {

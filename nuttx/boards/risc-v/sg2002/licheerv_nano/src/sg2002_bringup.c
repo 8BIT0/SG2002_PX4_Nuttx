faee2ed884096c32750b770eae78df15a8f255e1
8BIT0 <971168718@qq.com>
2026-01-14 22:33:45 +0800

    Changes to be committed: 	modified:   boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c

--- nuttx/boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c
+++ nuttx/boards/risc-v/sg2002/licheerv_nano/src/sg2002_bringup.c
@@ -14,8 +14,10 @@
 static struct i2c_master_s *sg2002_i2c_register(int bus) {
     struct i2c_master_s *i2c = sg2002_i2cbus_initialize(bus);
 
-    if (i2c)
+    if (i2c) {
+        sg2002_trace("Registering I2C bus %d\t addr 0x%08x\n", bus, (uint32_t)i2c);
         i2c_register(i2c, bus);
+    }
 
     return i2c;
 }
@@ -35,7 +37,11 @@ int sg2002_bringup(void) {
 
 #if defined(CONFIG_EEPROM) && defined(CONFIG_I2C_EE_24XX)
     if (eeprom_i2c != NULL)
-        ee24xx_initialize(eeprom_i2c, 0x50, "/dev/eeprom0", EEPROM_24XX64, 0);
+        if(ee24xx_initialize(eeprom_i2c, 0x50, "/dev/eeprom0", EEPROM_24XX64, 0) < 0) {
+            sg2002_trace("ERROR: Failed to initialize 24xx64 EEPROM driver\n");
+        } else {
+            sg2002_trace("24xx64 EEPROM driver initialized successfully\n");
+        }
 #endif
 
 #endif

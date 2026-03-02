0a50f6cf98ccd3eac2ab4ac27ec87a200ff74085
8BIT <971168718@qq.com>
2026-03-02 17:10:25 +0800

    Changes to be committed: 	modified:   nuttx/arch/risc-v/src/sg2002/hardware/sg2002_gpio.h
    1. gpio driver update

--- nuttx/arch/risc-v/src/sg2002/hardware/sg2002_gpio.h
+++ nuttx/arch/risc-v/src/sg2002/hardware/sg2002_gpio.h
@@ -1,73 +1,76 @@
 #ifndef __ARCH_RISCV_SRC_SG2002_HARDWARE_SG2002_GPIO_H
 #define __ARCH_RISCV_SRC_SG2002_HARDWARE_SG2002_GPIO_H
 
+#define SG2002_GPIOA_BASE                   0x03020000ul
+#define SG2002_GPIOB_BASE                   0x03021000ul
+#define SG2002_GPIOC_BASE                   0x03022000ul
+#define SG2002_GPIOD_BASE                   0x03023000ul
 
-#define SG2002_GPIOA_BASE 0x03020000ul
-#define SG2002_GPIOB_BASE 0x03021000ul
-#define SG2002_GPIOC_BASE 0x03022000ul
-#define SG2002_GPIOD_BASE 0x03023000ul
+/* register offset definition */
+#define SG2002_GPIO_SWPORTA_DR_OFFSET       0x00
+#define SG2002_GPIO_SWPORTA_DDR_OFFSET      0x04
+#define SG2002_GPIO_INTEN_OFFSET            0x30
+#define SG2002_GPIO_INTMASK_OFFSET          0x34
+#define SG2002_GPIO_INTTYPE_LEVEL_OFFSET    0x38
+#define SG2002_GPIO_INT_PRIORITY_OFFSET     0x3C
+#define SG2002_GPIO_INTSTATUS_OFFSET        0x40
+#define SG2002_GPIO_RAW_INTSTATUS_OFFSET    0x44
+#define SG2002_GPIO_DEBOUNCE_OFFSET         0x48
+#define SG2002_GPIO_PORTA_EOI_OFFSET        0x4C
+#define SG2002_GPIO_EXT_PORTA_OFFSET        0x50
+#define SG2002_GPIO_LS_SYNC_OFFSET          0x60
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_SWPortA_DR_Reg_TyeDef;
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_SWPortA_DDR_Reg_TypeDef;
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_IntEN_Reg_TypeDef;
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_IntMask_Reg_TypeDef;
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_IntType_Level_Reg_TypeDef;
 
-typedef union {
-    struct {
-        uint32_t : 1;
-        uint32_t reserved : 31;
-    } field;
-    
+typedef struct{
     uint32_t val;
 } SG2002_GPIO_Int_Priority_Reg_TypeDef;
 
+typedef struct{
+    uint32_t val;
+} SG2002_GPIO_IntStatus_Reg_TypeDef;    /* read only */
+
+typedef struct{
+    uint32_t val;
+} SG2002_GPIO_RawIntStatus_Reg_TypeDef; /* read only */
+
+typedef struct{
+    uint32_t val;
+} SG2002_GPIO_Debounce_Reg_TypeDef;
+
+typedef struct{
+    uint32_t val;
+} SG2002_GPIO_PortA_EOI_Reg_TypeDef;    /* write only */
+
+typedef struct{
+    uint32_t val;
+} SG2002_GPIO_EXT_PortA_Reg_TypeDef;    /* read only */
+
 typedef union {
     struct {
-        uint32_t : 1;
+        uint32_t sync : 1;
         uint32_t reserved : 31;
     } field;
 
     uint32_t val;
-} SG2002_GPIO_IntStatus_Reg_TypeDef;
+} SG2002_GPIO_LS_Sync_Reg_TypeDef;
 
 #endif
